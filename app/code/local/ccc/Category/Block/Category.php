4302a0725e5e7f625d86fe96fdec4dffa18f9e1a
krushalvataliya24 <krushalvataliya24@gmail.com>
2023-06-10 10:52:01 +0530

    complete 0.1.0 a,b

---
+++ app/code/local/ccc/Category/Block/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+class Ccc_Category_Block_Category extends Mage_Core_Block_Template
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function getFeaturedCatagories()
+    {
+        $attributeCode = 'featured_category';
+        $attribute = Mage::getSingleton('eav/config')->getAttribute('catalog_category', $attributeCode);
+        $optionId = $attribute->getSource()->getOptionId('Yes');
+
+        return Mage::getModel('catalog/category')
+        ->getCollection()
+        ->addAttributeToSelect('*')
+        ->addAttributeToFilter('featured_category', array('eq' => $optionId));
+    }
+
+}
